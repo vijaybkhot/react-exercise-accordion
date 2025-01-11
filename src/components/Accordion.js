b5a91be7097d52f5f02b727d71f8934586961981
Vijay Khot <vijaysinh.khot@gmail.com>
2025-01-11 13:49:53 -0500

    Lifting up state and children prop

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,31 +1,40 @@
 import { useState } from "react";
 
 export default function Accordion({ data }) {
+  const [currOpen, setCurrOpen] = useState(null);
+
+  function toggleIsOpen(id) {
+    setCurrOpen((prev) => (prev === id ? null : id));
+  }
+
   return (
     <div className="accordion">
       {data.map((el, index) => (
         <AccordionItem
           title={el.title}
-          text={el.text}
           num={index + 1}
           key={el.title}
-        />
+          onToggleIsOpen={toggleIsOpen}
+          currOpen={currOpen}
+        >
+          {el.text}
+        </AccordionItem>
       ))}
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
-  function toggleIsOpen() {
-    setIsOpen((isOpen) => !isOpen);
-  }
+function AccordionItem({ num, title, onToggleIsOpen, currOpen, children }) {
+  let isOpen = currOpen === num ? true : false;
   return (
-    <div className={`item ${isOpen ? "open" : ""}`} onClick={toggleIsOpen}>
+    <div
+      className={`item ${isOpen ? "open" : ""}`}
+      onClick={() => onToggleIsOpen(num)}
+    >
       <p className="number">{num < 9 ? `0${num}` : num}</p>
       <p className="text">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      <div className={`content-box`}>{isOpen ? text : ""}</div>
+      <div className={`content-box`}>{isOpen ? children : ""}</div>
     </div>
   );
 }
